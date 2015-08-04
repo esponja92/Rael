bf05e1740fb2594607ff48a6b6a9f6a61fc4f528
Hugo <hugobcmt@gmail.com>
2015-08-03 22:35:12 -0300

    Melhorado detalhes na formatacao dos templates

--- rael/blog/views.py
+++ rael/blog/views.py
@@ -7,7 +7,7 @@ from django.utils import timezone
 # Create your views here.
 def inicio(request):
 	if request.user.is_authenticated():
-		posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+		posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date').reverse()
 		return render(request, 'blog/post_list.html', {'posts':posts})
 	else:
 		return redirect('django.contrib.auth.views.login')
